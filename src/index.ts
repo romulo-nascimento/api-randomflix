651f18c34940c18fbe1bc2f3d3be4e369af2a67d
Romulo Nascimento <romulo.nascimento@hotmart.com>
2022-01-20 16:23:26 -0300

    Add config files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import { start } from './server'
-import './websocket/client'
 
 start()
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,21 @@
 import express from 'express'
-import { createServer } from 'http'
-import { Server, Socket } from 'socket.io'
-
+import cors from 'cors'
 import { routes } from './routes'
 import './database'
 
-const PORT = 3333
+const PORT = process.env.PORT || 3000
 
 const app = express()
-const http = createServer(app)
 
 app.use(express.json())
 app.use(routes)
+app.use(cors())
 
-const io = new Server(http, {
-    cors: {
-        origin: '*'
-    }
-})
-
-io.on('connection', (socket: Socket) => {
-    console.log('Connected', socket.id)
-})
 
 const start = () => {
-    http.listen(PORT, () => {
+    app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
     })
 }
 
-export { start, io }
+export { start }
